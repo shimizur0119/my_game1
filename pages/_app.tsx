74b7e0f01c98ff2481f9e1530bd7a9e64460461f
shimizur0119 <otsukaresamannsatabasa@gmail.com>
2021-08-17 15:36:16 +0900

    ベースが大体完成

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,6 @@
-import '../styles/globals.css'
+import 'destyle.css/destyle.css';
+import "nes.css/css/nes.min.css";
+import "../styles/common.scss"
 import type { AppProps } from 'next/app'
 
 function MyApp({ Component, pageProps }: AppProps) {
